a67f8ffceda98efe23045214058b1e5e980beb24
Christopher Cavell <cdcavell@gmail.com>
2022-03-30 04:12:52 -0500

    Initial Development

---
+++ Source/Web/Customizable.Menu/Controllers/ConfigureController.cs
@@ -0,0 +1,24 @@
+﻿using ClassLibrary.Data;
+using ClassLibrary.Mvc.Services.AppSettings;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customizable.Menu.Controllers
+{
+    public class ConfigureController : ApplicationBaseController<ConfigureController>
+    {
+        public ConfigureController(
+            ILogger<ConfigureController> logger,
+            IAppSettingsService appSettingsService,
+            IHttpContextAccessor httpContextAccessor,
+            ApplicationDbContext dbContext
+        ) : base(logger, appSettingsService, httpContextAccessor, dbContext)
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
