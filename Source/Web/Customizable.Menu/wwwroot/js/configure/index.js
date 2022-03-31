6a931271be43a9470bdcab027f9413bc4dc79836
Christopher Cavell <cdcavell@gmail.com>
2022-03-31 05:30:35 -0500

    Initial Development

--- Source/Web/Customizable.Menu/wwwroot/js/configure/index.js
+++ Source/Web/Customizable.Menu/wwwroot/js/configure/index.js
@@ -1,1 +1,18 @@
-﻿
+﻿$(document).ready(function () {
+
+    let Configure = function () {
+
+        Urls: {
+            Index: {
+                GetMenuList: "/Configure/GetMenuList"
+            }
+        }
+
+        function Index()  {
+
+            alert(Urls.Index.GetMenuList);
+        }
+
+    }
+
+});
